b4aac31cb12e0ae258efd32c10ad5cfcd057fb01
Simão Moreno <simaojmoreno@gmail.com>
2021-09-12 01:29:11 +0100

    Add files via upload

---
+++ RedeNeural/src/redeneural/RedeNeural.java
@@ -0,0 +1,17 @@
+package redeneural;
+
+/**
+ *
+ * @author Simao Moreno
+ */
+public class RedeNeural {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      MainFrame telaPrincipal = new MainFrame();
+      telaPrincipal.setVisible(true);
+    }
+    
+}
